31fdf8c2b8e3a764504796743694dbddc240e711
Mohanaprasath <mohanaprasath12345@gmail.com>
2022-04-20 12:51:55 -0400

    hh

--- src/navigation/Secondpage.js
+++ src/navigation/Secondpage.js
@@ -3,7 +3,7 @@ import {View, Text, Button} from 'react-native';
 function Secondpage({navigation}) {
   return (
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text style={{fontSize: 50}}>Page-2</Text>
+      <Text style={{fontSize: 50}}>Pages-2</Text>
       <Button title="back" onPress={() => navigation.navigate('First')} />
     </View>
   );
